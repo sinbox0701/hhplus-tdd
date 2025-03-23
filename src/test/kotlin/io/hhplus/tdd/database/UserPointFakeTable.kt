973b09ab9f096121197fc146110f7243a879931c
kiwoong1230 <kwpark@enki.co.kr>
2025-03-23 21:47:25 +0900

    Test Double - Fake 객체 추가

---
+++ src/test/kotlin/io/hhplus/tdd/database/UserPointFakeTable.kt
@@ -0,0 +1,18 @@
+package io.hhplus.tdd.database
+
+import io.hhplus.tdd.point.UserPoint
+
+class UserPointFakeTable: UserPointTable() {
+    private val table = HashMap<Long, UserPoint>()
+
+    override fun selectById(id: Long): UserPoint {
+        Thread.sleep(Math.random().toLong() * 200L)
+        return table[id] ?: UserPoint(id = id, point = 0, updateMillis = System.currentTimeMillis())
+    }
+    override fun insertOrUpdate(id: Long, amount: Long): UserPoint {
+        Thread.sleep(Math.random().toLong() * 300L)
+        val userPoint = UserPoint(id = id, point = amount, updateMillis = System.currentTimeMillis())
+        table[id] = userPoint
+        return userPoint
+    }
+}
