90ea4206dbf168bf133a766de0d2955b20b47490
kiwoong1230 <kwpark@enki.co.kr>
2025-03-28 03:13:13 +0900

    Unit Test

--- src/test/kotlin/io/hhplus/tdd/point/PointServiceImplTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointServiceImplTest.kt
@@ -1,27 +1,26 @@
 package io.hhplus.tdd.point
 
-import io.hhplus.tdd.database.PointHistoryFakeTable
-import io.hhplus.tdd.database.UserPointFakeTable
+import io.hhplus.tdd.database.PointHistoryTable
+import io.hhplus.tdd.database.UserPointTable
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
+import kotlin.random.Random
 
 class PointServiceImplTest {
-    private val userId = 1L
-
-    private val pointHistoryTable = PointHistoryFakeTable()
-    private val userPointTable = UserPointFakeTable()
+    private val pointHistoryTable = PointHistoryTable()
+    private val userPointTable = UserPointTable()
 
     private val sut: PointServiceImpl = PointServiceImpl(userPointTable,pointHistoryTable)
 
     @Test fun `입력 받은 id로 UserPoint 조회 시, 해당 id의 UserPoint 반환`(){
+        val userId = Random.nextLong()
         val userPoint = sut.findUserPointById(userId)
 
         assertEquals(userId, userPoint.id)
     }
 
     @Test fun `입력 받은 userId로 pointHistory 조회 시, 해당 userId를 가지고있는 pointHistory List 반환`(){
+        val userId = Random.nextLong()
         val pointHistories = sut.findUserPointHistoryByUserId(userId)
 
         for (pointHistory in pointHistories) {
@@ -30,6 +29,7 @@ class PointServiceImplTest {
     }
 
     @Test fun `충전 금액이 최소 충전 금액과 최대 충전 금액사이에 있고, 충전 후 금액이 최대 보유 가능 금액보다 작으면, 충전 금액만큼 포인트가 증가한다`() {
+        val userId = Random.nextLong()
         val userPoint = sut.findUserPointById(userId)
         val amount = (UserPoint.MIN_AMOUNT_PER_CHARGE..UserPoint.MAX_AMOUNT_PER_CHARGE - userPoint.point).random()
 
@@ -40,6 +40,7 @@ class PointServiceImplTest {
     }
 
     @Test fun `사용 금액이 최소 사용 금액과, 최대 사용 금액사이에 있고, 가지고 있는 point를 안 넘으면 사용 금액만큼 포인트가 감소한다`() {
+        val userId = Random.nextLong()
         val userPoint = sut.findUserPointById(userId)
         val amount = (UserPoint.MIN_AMOUNT_PER_USE..Math.min(UserPoint.MAX_AMOUNT_PER_USE, userPoint.point)).random()
 
